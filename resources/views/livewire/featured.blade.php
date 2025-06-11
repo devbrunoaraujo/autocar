9f9966703ad2d9523fb19e2ad909f3246bf38445
Bruno <dev.brunoaraujo@gmail.com>
2025-06-11 08:45:17 -0300

    .

--- resources/views/livewire/featured.blade.php
+++ resources/views/livewire/featured.blade.php
@@ -2,7 +2,7 @@
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
         @foreach ($cars as $car)
             <div class="bg-white rounded-xl shadow p-4">
-                <img src="{{ $car->thumb }}" class="w-full h-48 object-cover rounded" alt="{{ $car->modelo_nome }}">
+                <img src="{{ asset('storage/' . $car->thumb) }}" class="w-full h-48 object-cover rounded" alt="{{ $car->modelo_nome }}">
                 <h3 class="mt-2 text-lg font-bold">{{ $car->marca_nome }} {{ $car->modelo_nome }}</h3>
                 <p class="text-gray-600">{{ $car->ano_nome }} • {{ number_format($car->preco, 2, ',', '.') }}</p>
             </div>
--- resources/views/livewire/navbar.blade.php
+++ resources/views/livewire/navbar.blade.php
@@ -12,26 +12,25 @@
             </ul>
 
             {{-- Mobile menu toggle (Alpine.js recomendado) --}}
-            <div class="md:hidden">
-                <button @click="open = !open" x-data="{ open: false }" x-on:click="open = !open">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
-                        viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                            stroke-width="2" d="M4 6h16M4 12h16M4 18h16"/>
-                    </svg>
-                </button>
+           <div class="md:hidden" x-data="{ open: false }"> <!-- x-data movido para o contêiner -->
+    <button @click="open = !open">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none"
+            viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round"
+                stroke-width="2" d="M4 6h16M4 12h16M4 18h16"/>
+        </svg>
+    </button>
 
-                <div x-show="open" class="absolute top-16 left-0 w-full bg-white border-t shadow-md z-50">
-                    <ul class="flex flex-col gap-4 p-4">
-                        <li><a href="{{ route('home') }}">Início</a></li>
-                        <li><a href="{{ route('estoque') }}">Estoque</a></li>
-                        <li><a href="#sobre">Sobre</a></li>
-                        <li><a href="#depoimentos">Depoimentos</a></li>
-                        <li><a href="#contato">Contato</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </nav>
+    <div x-show="open" class="absolute top-16 left-0 w-full bg-white border-t shadow-md z-50">
+        <ul class="flex flex-col gap-4 p-4">
+            <li><a href="{{ route('home') }}">Início</a></li>
+            <li><a href="{{ route('estoque') }}">Estoque</a></li>
+            <li><a href="#sobre">Sobre</a></li>
+            <li><a href="#depoimentos">Depoimentos</a></li>
+            <li><a href="#contato">Contato</a></li>
+        </ul>
+    </div>
+</div>
+
 
 </div>
